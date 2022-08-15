fe1ebdf4d87678fc9e4115bfe36b751fa08474f3
Angela Milosheska <milosheskaa@gmail.com>
2022-08-15 21:01:12 +0200

    Initial commit

---
+++ appointment/src/test/java/mk/ukim/finki/emt/veterinary/appointment/AppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.veterinary.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ veterinary/src/test/java/mk/ukim/finki/emt/veterinary/veterinary/VeterinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.veterinary.veterinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
